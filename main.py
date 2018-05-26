a611153e9cd0549456e94a47e60de77ddacf3769
jlcoto <jscoto@gmail.com>
2018-05-26 16:22:27 +0200

    Add files

---
+++ main.py
@@ -0,0 +1,5 @@
+def add_me():
+	return 'yes'
+
+def not_me():
+	return 'no'
---
+++ test.py
@@ -0,0 +1,8 @@
+import main
+import pytest
+
+def test_function():
+    assert main.add_me() == 'yes'
+
+def test_function():
+    assert main.not_me() == 'no'
