aa22d2ee7c902425e43073705ed8f8591ad5982f
jlcoto <jscoto@gmail.com>
2018-05-26 16:25:59 +0200

    solve conflicts

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import main
 import pytest
 
@@ -7,11 +6,4 @@ def test_function():
 
 def test_function():
     assert main.not_me() == 'no'
-||||||| merged common ancestors
-=======
-import main
-import pytest
 
-def test_function():
-    assert main.add_me() == 'yes'
->>>>>>> 91bae4ce2e60eddb093f4c68074aa0a0fbb9bc0f
